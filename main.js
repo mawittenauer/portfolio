c6e014844525609b732e30cad8082c5504b0bbd2
Mike Wittenauer <mikewittenauerwebdevelopment@gmail.com>
2016-09-11 16:27:08 -0400

    Add mobile menu toggle

--- main.js
+++ main.js
@@ -1,3 +1,13 @@
 $(document).ready(function() {
+  $('#menu-out').on("click", function() {
+    $('body').animate({
+      left: '100px'
+    }, 100);
+  });
   
+  $('#menu-in').on("click", function() {
+    $('body').animate({
+      left: '0px'
+    }, 100)
+  });
 });
